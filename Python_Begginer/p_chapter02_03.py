9c104622c6996f2a7fbb1439025345f5eb4f21e8
Kimwonbin0921 <pransis1@naver.com>
2021-11-20 16:49:32 +0900

    Python - Class

--- Python_Begginer/p_chapter02_03.py
+++ Python_Begginer/p_chapter02_03.py
@@ -37,6 +37,22 @@ class Car:
     def get_price_culc(self): # 후 가격을 반환해주는 메소드
         return 'After Car Price -> company : {}, price : {}'.format(self._company, self._details.get('price') * Car.price_per_raise)
 
+    # Class Method : cls를 받는다.
+    @classmethod # class 메소드는 @classmethod를 선언하면서 작성 시작
+    def raise_price(cls, per): # class method의 각 인자는 self인 인스턴스가 아닌 cls라는 각 클래스를 받는다. 
+        if per <= 1:
+            print('Please Enter 1 Or More')
+            return
+        cls.price_per_raise = per # 이처럼 cls.(클래스변수) = (인자)를 선언한다.
+        print('Succeed! price increased.')
+
+    # Static Method : 아무것도 받지 않는다.
+    @staticmethod # static 메소드는 @staticmethod를 선언하면서 작성 시작
+    def is_bmw(inst):
+        if inst._company == 'Bmw':
+            return 'OK! This car is {}'.format(inst._company)
+        return 'Sorry. This car is not Bmw.'
+
 car1 = Car('Ferrari', {'color': 'White', 'horsepower': 400, 'price': 8000})
 car2 = Car('Bmw', {'color': 'Black', 'horsepower': 270, 'price': 5000})
 
@@ -53,8 +69,20 @@ print(car1.get_price())
 print(car2.get_price())
 
 # 가격 인상 (클래스 메소드 미사용)
-Car.price_per_raise = 1.4
+Car.price_per_raise = 1.4 # class 메소드를 사용하지 않고 직접 접근한 방식이므로 그리 좋은 방식은 아니다.
 
 # 가격 인상 후
 print(car1.get_price_culc())
 print(car2.get_price_culc())
+
+# 클래스 메소드 사용 가격 인상 : 클래스 변수를 사용할 때는 클래스 메소드를 사용하여 핸들링 하는 것이 효율적이다.
+Car.raise_price(1.6)
+
+# 가격 인상 후
+print(car1.get_price_culc())
+print(car2.get_price_culc())
+
+# 스태이틱 메소드 : 유연한 것이 특징
+print(car1.is_bmw(car1))
+print(car2.is_bmw(car2))
+print(Car.is_bmw(car2)) # 클래스로 바로 호출도 가능하다.
